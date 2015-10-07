a6bfd562456fc15211e002fc8f3ebbac3d7e4ba3
Bjørge Næss <bjoerge@gmail.com>
2015-10-07 23:19:12 +0200

    Increase max-params

--- base.js
+++ base.js
@@ -270,7 +270,7 @@ module.exports = {
     "wrap-regex": 0,
     "max-depth": [2, 2],
     "max-len": [1, 120, 4],
-    "max-params": [2, 3],
+    "max-params": [2, 5],
     "max-statements": [1, 50],
     "block-spacing": [2, 'always'],
     "global-require": 0,
