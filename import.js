2bc363398a881415d76dc532671cd75ea8a8f156
Bjørge Næss <bjoerge@gmail.com>
2015-12-08 14:46:07 +0100

    Add rules for eslint-plugin-import

---
+++ import.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "rules": {
+    "import/default": 0,
+    "import/export": 2,
+    "import/imports-first": 2,
+    "import/namespace": 2,
+    "import/named": 2,
+    "import/no-duplicates": 2,
+    "import/no-named-as-default": 2,
+    "import/no-require": 2,
+    "import/no-unresolved": 2
+  }
+};
