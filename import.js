c118c28ca5f1d16788e61c44076d05cb10b7cb4f
Bjørge Næss <bjoerge@gmail.com>
2016-02-25 11:16:38 +0100

    Update rules for eslint-plugin import

--- import.js
+++ import.js
@@ -8,9 +8,10 @@ module.exports = {
     "import/imports-first": 2,
     "import/namespace": 2,
     "import/named": 2,
+    "import/no-amd": 2,
+    "import/no-commonjs": 2,
     "import/no-duplicates": 2,
     "import/no-named-as-default": 2,
-    "import/no-require": 2,
     "import/no-unresolved": 2
   },
   "settings": {
